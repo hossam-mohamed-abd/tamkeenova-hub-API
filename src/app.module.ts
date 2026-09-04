33ddb5ed3f017342ec064b3693c136f9727ca87e
hossam-mohamed-abd <hossam545mohamed@gmail.com>
2026-09-04 18:52:27 +0300

    fix vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,31 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+// import { Module } from '@nestjs/common';
+// import { ConfigModule } from '@nestjs/config';
 
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { MailModule } from './modules/mail/mail.module';
-import { TestModule } from './modules/test/test.module';
-import { LoggerModule } from './common/logger/logger.module';
+// import { PrismaModule } from './prisma/prisma.module';
+// import { AuthModule } from './modules/auth/auth.module';
+// import { MailModule } from './modules/mail/mail.module';
+// import { TestModule } from './modules/test/test.module';
+// import { LoggerModule } from './common/logger/logger.module';
+// import { AppController } from './app.controller';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//     }),
+
+//     PrismaModule,
+//     AuthModule,
+//     MailModule,
+//   ],
+//   controllers: [AppController],
+// })
+// export class AppModule {}
+
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
-    PrismaModule,
-    AuthModule,
-    MailModule,
-  ],
   controllers: [AppController],
 })
 export class AppModule {}
